d974c7278bb0250639ee44406a6d92732ed7ca9a
sengbh <sengbh@ieee.org>
2019-04-08 17:47:27 -0500

    update 1st part

--- lib/main.dart
+++ lib/main.dart
@@ -3,127 +3,54 @@ import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
-
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Welcome to flutter app',
-      home: RandomWords(),
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.green,
-      ),
-      //home: MyHomePage(title: 'Welcome to flutter app'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  final StreamController<void>buttonPressStream;
-  MyHomePage({Key key, this.title}) : super(key: key) ({this.buttonPressStream});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState(buttonPressStream);
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  StreamController<void> buttonPressStream;
-  _MyHomePageState(this.buttonPressStream);
-  // int _counter = 0;
-
-  // void _incrementCounter() {
-  //   setState(() {
-  //     // This call to setState tells the Flutter framework that something has
-  //     // changed in this State, which causes it to rerun the build method below
-  //     // so that the display can reflect the updated values. If we changed
-  //     // _counter without calling setState(), then the build method would not be
-  //     // called again, and so nothing would appear to happen.
-  //     _counter++;
-  //   });
-  // }
-
   @override
   Widget build(BuildContext context) {
     //final wordPair = WordPair.random();
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    
-      return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return MaterialApp(
+      //title: 'Welcome to Flutter',
+      home: RandomWords(),
+      theme: ThemeData(
+        primarySwatch: Colors.green,
       ),
-      body: _buildSuggestions();
-      );
-    }
-
-    @override
-    void initState(){
-      super.initState();
-      buttonPressStream.stream.listen((_){
-        setState(() {
-          
-        });
-      });
-    }
-}   
+    );
+  }
+}
 
 class RandomWords extends StatefulWidget {
-    StreamController<void> buttonPressStream = StreamController<bool>.broadcast();
-    @override
-    RandomWordsState createState() => new RandomWordsState();
+  @override
+  RandomWordsState createState() => new RandomWordsState();
 }
 
 class RandomWordsState extends State<RandomWords> {
-  StreamController<void> buttonPressStream;
-  RandomWordsState(this.buttonPressStream);
   final _suggestions = <WordPair>[];
-  final _biggestFont = const TextStyle(fontSize: 18.0);
+  final _biggestFont = const TextStyle(fontSize: 18);
   @override
-  Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    return Text(wordPair.asPascalCase);
+  Widget build(BuildContext context){
+    // final wordPair = WordPair.random();
+    // return Text(wordPair.asPascalCase);
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title:
+          Text('Generate Words'),
+      ),    
+      body: _buildSuggestion(),
+    );
   }
-
-  Widget _buildSuggestions() {
+  
+  Widget _buildSuggestion() {
     return ListView.builder(
       padding: const EdgeInsets.all(16.0),
       itemBuilder: (context, i) {
-        if (i.isOdd) return Divider();
+        if(i.isOdd) return Divider();
 
         final index = i ~/ 2;
-        if(index >= _suggestions.length){
+        if(index >= _suggestions.length) {
           _suggestions.addAll(generateWordPairs().take(10));
         }
         return _buildRow(_suggestions[index]);
-      }
-    );
+      });
   }
 
   Widget _buildRow(WordPair pair) {
@@ -134,4 +61,5 @@ class RandomWordsState extends State<RandomWords> {
       ),
     );
   }
+
 }
