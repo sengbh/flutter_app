f1098b7fc8431c395fd90f51fe2c50cc36564349
sengbh <sengbh@ieee.org>
2019-04-16 12:01:19 -0500

    mod version

--- lib/main.dart
+++ lib/main.dart
@@ -23,43 +23,92 @@ class RandomWords extends StatefulWidget {
 }
 
 class RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
-  final _biggestFont = const TextStyle(fontSize: 18);
+  final List<WordPair>  _suggestions = <WordPair>[];
+  final Set<WordPair> _saved = new Set<WordPair>();
+  final TextStyle _biggestFont = const TextStyle(fontSize: 18);
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context){ 
     // final wordPair = WordPair.random();
     // return Text(wordPair.asPascalCase);
     return Scaffold(
-      appBar: AppBar(
+      appBar: new AppBar(
         centerTitle: true,
         title:
           Text('Generate Words'),
-      ),    
-      body: _buildSuggestion(),
-    );
-  }
-  
-  Widget _buildSuggestion() {
-    return ListView.builder(
-      padding: const EdgeInsets.all(16.0),
-      itemBuilder: (context, i) {
-        if(i.isOdd) return Divider();
+        actions: <Widget>[
+          new IconButton(icon: const Icon(Icons.list), onPressed: _pushSaved),
+                  ],   
+                ),
+                body: _buildSuggestion(),
+              );
+            } 
+            
+            Widget _buildSuggestion() {
+              return ListView.builder(
+                padding: const EdgeInsets.all(16.0),
+                itemBuilder: (context, i) {
+                  if(i.isOdd) return Divider();
+          
+                  final index = i ~/ 2;
+                  if(index >= _suggestions.length) {
+                    _suggestions.addAll(generateWordPairs().take(10));
+                  }
+                  return _buildRow(_suggestions[index]);
+                });
+            }
+          
+            Widget _buildRow(WordPair pair) {
+              final bool alreadySaved = _saved.contains(pair);  
+              return ListTile(
+                title: Text(
+                  pair.asPascalCase,
+                  style: _biggestFont,
+                ),
+                trailing: new Icon(
+                  alreadySaved ? Icons.favorite : Icons.favorite_border,
+                  color: alreadySaved ? Colors.red : null, 
+                ),
+                onTap: () {
+                  setState(() {
+                    if (alreadySaved) {
+                      _saved.remove(pair);
+                    } else
+                      _saved.add(pair);
+                  });
+                },
+              );
+            }
+          
+          
+            void _pushSaved() {
+              Navigator.of(context).push(
+                new MaterialPageRoute<void>(
+                  builder: (BuildContext context) {
+                    final Iterable<ListTile> tiles = _saved.map(
+                      (WordPair pair) {
+                        return new ListTile(
+                          title: new Text(
+                            pair.asPascalCase, 
+                            style: _biggestFont,
+                          ),
+                        );
+                      },
+                    );
+                    final List<Widget> divided = ListTile.divideTiles(
+                      context: context,
+                      tiles: tiles,
+                    ).toList();
 
-        final index = i ~/ 2;
-        if(index >= _suggestions.length) {
-          _suggestions.addAll(generateWordPairs().take(10));
-        }
-        return _buildRow(_suggestions[index]);
-      });
-  }
-
-  Widget _buildRow(WordPair pair) {
-    return ListTile(
-      title: Text(
-        pair.asPascalCase,
-        style: _biggestFont,
-      ),
-    );
-  }
-
+                    return new Scaffold(
+                      appBar: new AppBar(
+                        title: const Text('Save Suggestions'),
+                      ),
+                      body: new ListView(
+                        children: divided
+                      ),
+                    );
+                  },
+                ),
+              );
+            }
 }
